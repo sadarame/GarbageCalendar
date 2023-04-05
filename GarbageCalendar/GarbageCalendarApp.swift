18a8b9e324b3282442bd673ddbf49394ed4b6644
yousuke <sadadarame@gmail.com>
2023-04-05 15:59:36 +0900

    Initial Commit

---
+++ GarbageCalendar/GarbageCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarbageCalendarApp.swift
+//  GarbageCalendar
+//
+//  Created by Yosuke Yoshida on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct GarbageCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
